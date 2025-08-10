2d0f943990a9519fecaf8a7d9cf48347ea696ac7
Debashis470 <debasisdhir7@gmail.com>
2025-08-10 11:11:42 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-   class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    extra_password = models.CharField(max_length=128, blank=True, null=True)  # custom password field
 
     def __str__(self):
         return self.user.username
-   
